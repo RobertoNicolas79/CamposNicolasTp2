b8ba12db7298fb07457f4dbdc8fdc99f7cc936f5
RobertoNicolas79 <agusstinn1979@gmail.com>
2021-04-29 10:01:24 -0300

    Commit tp2

---
+++ Tp2/ArbolBinario.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tp2
+{
+	public class ArbolBinario<T>
+	{
+		
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+	
+		
+		public ArbolBinario(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public ArbolBinario<T> getHijoIzquierdo() {
+			return this.hijoIzquierdo;
+		}
+	
+		public ArbolBinario<T> getHijoDerecho() {
+			return this.hijoDerecho;
+		}
+	
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+			this.hijoIzquierdo=hijo;
+		}
+	
+		public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+			this.hijoDerecho=hijo;
+		}
+	
+		public void eliminarHijoIzquierdo() {
+			this.hijoIzquierdo=null;
+		}
+	
+		public void eliminarHijoDerecho() {
+			this.hijoDerecho=null;
+		}
+	
+		public bool esHoja() {
+			return this.hijoIzquierdo==null && this.hijoDerecho==null;
+		}
+		
+		public void inorden() {
+		}
+		
+		public void preOrden() {
+			Console.Write(this.getDatoRaiz()+"|");
+			
+			if(this.getHijoIzquierdo()!=null){
+				this.getHijoIzquierdo().preOrden();
+			}
+			
+			if(this.getHijoDerecho()!=null){
+				this.getHijoDerecho().preOrden();
+			}
+		}
+		
+		public void postorden() {
+		}
+		
+		public void recorridoPorNiveles() {
+		}
+	
+		public int contarHojas(int hojas) {
+			
+			
+			if(this.getHijoIzquierdo()==null && this.getHijoDerecho()==null){
+				hojas++;
+			}
+			
+			if(this.getHijoIzquierdo()!=null){
+				hojas=this.getHijoIzquierdo().contarHojas(hojas);
+			}
+			
+			if(this.getHijoDerecho()!=null){
+				hojas=this.getHijoDerecho().contarHojas(hojas);
+			}
+			
+			return hojas;
+			
+		}
+		
+		public void recorridoEntreNiveles(int n,int m) {
+		}
+	}
+}
---
+++ Tp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace tp2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+           
+			ArbolBinario<int> arbolBinarioA = new ArbolBinario<int>(1);
+
+			ArbolBinario<int> hijoIzquierdo=new ArbolBinario<int>(2);
+			hijoIzquierdo.agregarHijoIzquierdo(new ArbolBinario<int>(3));
+			hijoIzquierdo.agregarHijoDerecho(new ArbolBinario<int>(4));
+			
+			ArbolBinario<int> hijoDerecho=new ArbolBinario<int>(5);
+			hijoDerecho.agregarHijoIzquierdo(new ArbolBinario<int>(6));
+			hijoDerecho.agregarHijoDerecho(new ArbolBinario<int>(7));
+			
+			arbolBinarioA.agregarHijoIzquierdo(hijoIzquierdo);
+			arbolBinarioA.agregarHijoDerecho(hijoDerecho);
+
+			arbolBinarioA.preOrden();
+			//arbolBinarioA.inorden();
+			//arbolBinarioA.postorden();
+			//arbolBinarioA.recorridoPorNiveles();
+			
+			int hoja=0;
+			Console.WriteLine("\nCantidad de Hojas: "+arbolBinarioA.contarHojas(hoja));
+			
+			Console.WriteLine("\nPresione una tecla para concluir la ejecucion . . . ");
+			Console.ReadKey(true);
+			
+			
+		}
+	}
+}
